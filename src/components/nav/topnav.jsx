4c1bdfc0d991b88804b73fef913940661bad9245
Alex Eaves <eavesalex1@gmail.com>
2024-05-01 21:48:56 +0100

    Amended 'Search By Dropdown' links & hover

--- src/components/nav/topnav.jsx
+++ src/components/nav/topnav.jsx
@@ -17,15 +17,15 @@ export const TopNav = () => {
               </div>
               <li class="department">Search By Department<svg xmlns="http://www.w3.org/2000/svg" className="caret" viewBox="0 0 320 512"><path d="M137.4 374.6c12.5 12.5 32.8 12.5 45.3 0l128-128c9.2-9.2 11.9-22.9 6.9-34.9s-16.6-19.8-29.6-19.8L32 192c-12.9 0-24.6 7.8-29.6 19.8s-2.2 25.7 6.9 34.9l128 128z"/></svg>
                 <ul class="dropdown">
-                  <li><a href="/">All Accessories</a></li>
-                  <li><a href="/">Computers</a></li>
-                  <li><a href="/">Digital Media</a></li>
-                  <li><a href="/">Electronics</a></li>
-                  <li><a href="/">Mobile Phones</a></li>
-                  <li><a href="/">PC Accessories</a></li>
-                  <li><a href="/">Photo</a></li>
+                  <li><a href="/" className='dropdownLink'>All Accessories</a></li>
+                  <li><a href="/" className='dropdownLink'>Computers</a></li>
+                  <li><a href="/" className='dropdownLink'>Digital Media</a></li>
+                  <li><a href="/" className='dropdownLink'>Electronics</a></li>
+                  <li><a href="/" className='dropdownLink'>Mobile Phones</a></li>
+                  <li><a href="/" className='dropdownLink'>PC Accessories</a></li>
+                  <li><a href="/" className='dropdownLink'>Photo</a></li>
                   <p class="divider"></p>
-                  <li class="afterDivider"><a href="/">All Products</a></li>
+                  <li class="afterDivider"><a href="/" className='dropdownLink'>All Products</a></li>
                 </ul>
               </li>
               <div className="searchBar">
