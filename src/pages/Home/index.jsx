5fa64b2c638ac74b9df063958f4c06166b1f5b2e
Alex Eaves <eavesalex1@gmail.com>
2024-05-22 18:09:37 +0100

    body background amended

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,6 +1,7 @@
 import Footer from '../../components/footer/footer';
 import TopNav from '../../components/nav/topnav';
 import Banner from './banner';
+import './home.css';
 
 function HomePage() {
   return (
