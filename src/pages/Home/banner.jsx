cda5d57778fedb279e54b759d63eb9cbaebb1d5a
Alex Eaves <49497029+mraeaves@users.noreply.github.com>
2024-06-17 16:59:48 +0100

    Update banner.jsx

--- src/pages/Home/banner.jsx
+++ src/pages/Home/banner.jsx
@@ -8,7 +8,7 @@ const Banner = () => {
             <div className='bannerContent'>
                 <p><span className='limitedTitle'>Limited Deals</span></p>
                 <h3>Samsung Note 9 from £719. Apple iPad Pro 2019 from £999</h3>
-                <h5>Trade in your old devices for a new one</h5>
+                <h5>Trade in your old device for a new one</h5>
                 <a href='/' className='bannerButton'>Learn More</a>
             </div>
         </div>
