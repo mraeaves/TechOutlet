54549c768e1fa1dfeda5bd2d7115fc41b7b89b3e
Alex Eaves <49497029+mraeaves@users.noreply.github.com>
2024-07-01 20:57:06 +0100

    Update 404 page text

--- src/pages/404/index.jsx
+++ src/pages/404/index.jsx
@@ -9,7 +9,7 @@ function NoPage() {
     <div className='Page404'>
       <div className='errorContent'>
         <h1>404</h1>
-        <p>It appears the page you are looking for can't be found</p>
+        <p>We can't seem to find the page you're looking for.</p>
         <a href='/'>Go To Homepage<i class="fa fa-arrow-circle-right home-arrow-right" aria-hidden="true"></i></a>
       </div>
     </div>
