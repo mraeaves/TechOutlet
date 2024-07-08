daddc973d65e766c0289653578d61c2764e03ec7
Alex Eaves <49497029+mraeaves@users.noreply.github.com>
2024-07-08 10:42:27 +0100

    Update footer email

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => {
   return (
     <div className='footer'>
         <p className='left'>Powered By TechOutlet</p>
-        <p className='right'><i class="fa fa-envelope-o mail-red" aria-hidden="true"></i>contact-us@techoutlet.co.uk</p>
+        <p className='right'><i class="fa fa-envelope-o mail-red" aria-hidden="true"></i>support@techoutlet.co.uk</p>
     </div>
   )
 }
