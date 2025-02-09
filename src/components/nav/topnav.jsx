dc1175cb9c0c9009ff7814d86255e78233bdba2a
mraeaves <eavesalex1@gmail.com>
2025-02-09 10:36:25 +0000

    Dropdown in NAV updated to alphabetical order

--- src/components/nav/topnav.jsx
+++ src/components/nav/topnav.jsx
@@ -18,12 +18,12 @@ export const TopNav = () => {
               <li class="department">Search By Department<svg xmlns="http://www.w3.org/2000/svg" className="caret" viewBox="0 0 320 512"><path d="M137.4 374.6c12.5 12.5 32.8 12.5 45.3 0l128-128c9.2-9.2 11.9-22.9 6.9-34.9s-16.6-19.8-29.6-19.8L32 192c-12.9 0-24.6 7.8-29.6 19.8s-2.2 25.7 6.9 34.9l128 128z"/></svg>
                 <ul class="dropdown">
                   <li><a href="/" className='dropdownLink'>3D Printers</a></li>
+                  <li><a href="/" className='dropdownLink'>Accessories</a></li>
+                  <li><a href="/" className='dropdownLink'>Audio Devices</a></li>
+                  <li><a href="/" className='dropdownLink'>Cameras</a></li>
                   <li><a href="/" className='dropdownLink'>Computers</a></li>
                   <li><a href="/" className='dropdownLink'>Mobile Phones</a></li>
                   <li><a href="/" className='dropdownLink'>Printers</a></li>
-                  <li><a href="/" className='dropdownLink'>Audio Devices</a></li>
-                  <li><a href="/" className='dropdownLink'>Cameras</a></li>
-                  <li><a href="/" className='dropdownLink'>Accessories</a></li>
                   <p class="divider"></p>
                   <li class="afterDivider"><a href="/Products" className='dropdownLink'>All Products</a></li>
                 </ul>
